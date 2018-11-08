9b496f201af53cc937ba975eb61e830a2a1b657f
unknown <96micko@live.com>
2018-11-08 12:22:49 +0100

    Cetvrtak

--- ProjektniZadatak22/Manager/ClientCertValidator.cs
+++ ProjektniZadatak22/Manager/ClientCertValidator.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.IdentityModel.Selectors;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Manager
 {
-    public class ServiceCertValidator : X509CertificateValidator
+    public class ClientCertValidator : X509CertificateValidator
     {
+        public override void Validate(X509Certificate2 certificate)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- ProjektniZadatak22/PubSubEngine/Program.cs
+++ ProjektniZadatak22/PubSubEngine/Program.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using System.ServiceModel.Security;
 using System.Security.Cryptography.X509Certificates;
 using Manager;
+using System.Security.Principal;
 
 namespace PubSubEngine
 {
@@ -15,6 +16,8 @@ namespace PubSubEngine
     {
         static void Main(string[] args)
         {
+            string srvCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
+            Console.WriteLine(srvCertCN);
             NetTcpBinding binding = new NetTcpBinding();
             binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
 
@@ -24,9 +27,22 @@ namespace PubSubEngine
             host.AddServiceEndpoint(typeof(IPubSubEngine), binding, address);
 
             host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.Custom;
-            host.Credentials.ClientCertificate.Authentication.CustomCertificateValidator = new ServiceCertValidator();
+            //host.Credentials.ClientCertificate.Authentication.CustomCertificateValidator = new ClientCertValidator();
+            host.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+            host.Credentials.ServiceCertificate.Certificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, srvCertCN);
 
+            //host.Credentials.ServiceCertificate.Certificate = CertManager.GetCertificateFromFile("PubSubEngine.pfx");
+
+
+            try
+            {
             host.Open();
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         
             Console.WriteLine("PubSubEngine is open. Press <enter> to finish...");
             Console.ReadLine();
